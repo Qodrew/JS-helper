7f737a980c682f1a7e04e84a6b06b2c7913ac7be
Qodrew <120235371+Qodrew@users.noreply.github.com>
2023-03-24 16:09:33 +0300

    Update SliderCarusel.js

--- SliderCarusel.js
+++ SliderCarusel.js
@@ -106,3 +106,70 @@ const slides = document.querySelectorAll('.offer__slide'),
     // })
     
     // 86 slider (simple)
+
+    // 88 dots_slider
+    const offerSlider = document.querySelector(".offer__slider");
+    offerSlider.style.position = "relative";
+    const caruselIndic = document.createElement("ol"),
+        dots = []; // для переключения слайдов
+    caruselIndic.classList.add("carosel-indicators");
+    caruselIndic.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    offerSlider.append(caruselIndic);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement("li");
+        dot.setAttribute("data-slide-to", i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        caruselIndic.append(dot);
+        dots.push(dot);
+    }
+
+    dots.forEach((dot) => {
+        dot.addEventListener("click", (e) => {
+            const slideTo = e.target.getAttribute("data-slide-to");
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+            
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach((dot) => (dot.style.opacity = ".5"));
+            dots[slideIndex - 1].style.opacity = 1; // для выделения
+        });
+    });
+
+    // 88 dots_slider
